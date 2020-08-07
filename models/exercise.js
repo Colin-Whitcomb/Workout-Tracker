23bdc7692b04910401ef89935de4efd04ad72128
Colin Whitcomb <colinsw@berkeley.edu>
2020-08-07 09:35:31 -0700

    [add] to schema.

--- models/exercise.js
+++ models/exercise.js
@@ -3,50 +3,53 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const exerciseSchema = new Schema({
-  workouts: [
-    {
-      type: {
-        type: String,
-        trim: true,
-        // required: "Enter exercise type"
-      },
-      name: {
-        type: String,
-        trim: true,
-        // required: "Enter exercise name"
-      },
-      duration: {
-        type: Number,
-        // required: "Enter exercise duration"
+  workouts: [{
+    type: {
+      type: String,
+      trim: true,
+      // required: "Enter exercise type"
+    },
+    name: {
+      type: String,
+      trim: true,
+      // required: "Enter exercise name"
+    },
+    duration: {
+      type: Number,
+      // required: "Enter exercise duration"
 
-      },
-      weight: {
-        type: Number,
+    },
+    weight: {
+      type: Number,
 
-      },
-      reps: {
-        type: Number,
+    },
+    reps: {
+      type: Number,
 
-      },
-      sets: {
-        type: Number,
+    },
+    sets: {
+      type: Number,
 
-      },
-      distance: {
-        type: Number,
+    },
+    distance: {
+      type: Number,
 
-      },
-    }
-  ],
+    },
+  }],
   day: {
     type: Date,
     default: Date.now,
   },
-},
-  {
-    toJSON: {
-      virtuals: true,
+}, {
+  toJSON: {
+    virtuals: true,
   }
+});
+
+exerciseSchema.virtual("totalDuration").get(function () {
+  return this.workouts.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
 });
 
 const Exercise = mongoose.model("Exercise", exerciseSchema);
