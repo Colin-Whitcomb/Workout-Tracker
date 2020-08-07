13b1fb2162c3b866207578e9212a95cb91291ef1
Colin Whitcomb <colinsw@berkeley.edu>
2020-08-06 18:58:44 -0700

    [moved] file structure.

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,29 @@
+var path = require("path"); 
+
+// / ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // HTML GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases the user is shown an HTML page of content
+  // ---------------------------------------------------------------------------
+
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../Develop/public/index.html"));
+  });
+
+//   app.get("/exercise", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../Develop/public/exercise.html"));
+//   });
+
+//   app.get("/resources", (red, res) => {
+//     res.sendFile(path.join(__dirname, "../Resources/Resume.pdf"))
+//   })
+
+  // If no matching route is found default to home
+//   app.get("*", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../Develop/public/html/index.html"));
+//   });
+};
