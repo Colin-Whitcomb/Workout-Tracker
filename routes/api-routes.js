71b85b80d49fa310a026742a026a948a3bde173f
Colin Whitcomb <colinsw@berkeley.edu>
2020-08-07 05:49:52 -0700

    [add] POST request.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,12 +12,24 @@ app.get("/api/workouts", (req, res) => {
 
 // PUT /api/workouts
 app.put('/api/workout/:id', (req, res) => {
-    Exercise.findByIdAndUpdate(req.params.id, )
+    Exercise.findByIdAndUpdate(req.params.id, {$push: { workouts: req.body} }. {new: true})
+    .then( (workout) => {
+        console.log("Workout = " + workout);
+        res.send(workout);
+    })
 });
 
 // POST /api/workouts
-
+app.post('/api/workouts', (req, res) => {
+    Exercise.create(req.body)
+    .then( (workout) => {
+    console.log("Workout = " + workout);
+    res.send(workout);
+    })
+})
 
 // GET /api/workouts/range
-
+app.get('api/workouts/range', (req, res) => {
+    Exercise.find({})
+})
 module.exports = app;
